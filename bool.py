2395ced27aec4cde5bba3e01c16efc8291c9130b
Gerald Erickson <geraldraymonderickson@gmail.com>
2016-10-11 08:57:39 -0400

    Create bool.py

---
+++ bool.py
@@ -0,0 +1,23 @@
+# Ask for first and second numbers from the user before we define anything
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+
+#Create the comparison function that denotes what the assignment asked for
+def comparison(a, b):
+    if a > b:
+        return 1 #print 1 if a is greater than b
+    elif a == b:
+        return 0 #print 0 if a = b
+    else:
+        return -1 #print -1 for everything else, which is a > b
+
+# print the results of the user input's compare function
+print('If your first number is greater than your second number')
+print('then print 1. If your first number is less than your second')
+print('number, then print -1. If your first number is equal to your')
+print('second number, then print 0 \n', comparison(num1, num2))
+
+# print out the results of the arguments given in the assignment
+print('Test per the assignment a > b while a = 5 and b = 2:', comparison(5, 2))
+print('Test per the assignment a < b while a = 2 and b = 5:', comparison(2, 5))
+print('Test per the assignment a = b while a = 3 and b = 3:', comparison(3, 3))
